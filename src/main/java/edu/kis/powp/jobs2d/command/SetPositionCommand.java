7dd3fa2f8f83f4018790651f0049d26ad5562857
adam96212 <48412413+adam96212@users.noreply.github.com>
2019-10-27 20:10:59 +0100

    Lab3 - Command

---
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -0,0 +1,26 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand{
+		
+	
+	int x;
+	int y;
+	
+	
+	Job2dDriver job2dDriver;
+	
+	 public SetPositionCommand(Job2dDriver j) {
+		// TODO Auto-generated constructor stub
+		 job2dDriver = j;
+	}
+	
+	@Override
+	public void execute()
+	{
+	
+		job2dDriver.setPosition(x, y);
+	}
+	
+}
